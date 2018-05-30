a039743451f6a5481129f6311fc58912f25e48e4
Jared Horne <thomhorne@gmail.com>
2018-05-29 21:55:30 -0500

    added App.js

---
+++ hornes-music-service/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
