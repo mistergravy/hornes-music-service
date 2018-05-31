c7955b4ac14e0576cd152d8e75de39fca3fdc1ed
Jared Horne <thomhorne@gmail.com>
2018-05-30 23:41:32 -0500

    nearly have webpack working. still have problem with module not being found

--- hornes-music-service/src/index.js
+++ hornes-music-service/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
-// import AppRouter from './routers/index.js'
-// import '../node_modules/bootswatch/dist/materia/bootstrap.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import AppRouter from './routers/index.js'
+import '../node_modules/bootswatch/dist/materia/bootstrap.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<AppRouter />, document.getElementById('root'))
--- hornes-music-service/webpack.config.js
+++ hornes-music-service/webpack.config.js
@@ -2,6 +2,8 @@ var path = require('path');
 const webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+var DIST_DIR = path.join(__dirname, '/dist');
+
 module.exports = {
   entry: [
     './src/index.js'
@@ -13,14 +15,14 @@ module.exports = {
       // },
       { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']}
+      { test: /\.(png|svg|jpg|gif|woff|eot|ttf|woff2)$/, use: ['file-loader']}
     ]
   },
   resolve: {
     extensions: ['*', '.js', '.jsx']
   },
   output: {
-    path: __dirname + '/dist',
+    path: DIST_DIR,
     publicPath: '/',
     filename: 'bundle.js'
   },
